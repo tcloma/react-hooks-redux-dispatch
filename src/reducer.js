7f0aa842e5679acddb07f2f15e590a06b3865cf6
Tyrone <tyrone.cloma0219@gmail.com>
2022-09-06 13:42:10 -0400

    solution

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,7 @@
-function changeState(state, action) {
+let state = { count: 0 };
+let action = { type: "counter/increment" };
+
+const changeState = (state, action) => {
   switch (action.type) {
     case "counter/increment":
       return { count: state.count + 1 };
@@ -7,7 +10,26 @@ function changeState(state, action) {
   }
 }
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+const dispatch = (action) => {
+  state = changeState(state, action)
+  render()
+  return state;
+}
 
-changeState(state, action);
+const statefulh1 = document.createElement('h1')
+const render = () => {
+  const app = document.querySelector('#app')
+  statefulh1.textContent = state.count
+  app.append(statefulh1)
+}
+
+const button = document.getElementById('inc')
+inc.addEventListener('click', () => {
+  dispatch(action).count
+})
+
+render()
+
+// console.log('Count is: ',dispatch(action).count)
+// console.log('Count is: ',dispatch(action).count)
+// console.log('Count is: ',dispatch(action).count)
